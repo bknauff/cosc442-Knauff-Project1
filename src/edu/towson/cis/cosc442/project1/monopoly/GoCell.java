6df7e7a0dd16eb98affd0c4b8015e01c8b527968
bknauffgit config --global user.name bknauff <bkjr88@verizon.net>
2017-02-17 13:12:18 -0500

    Task 14 Changing a Method's Signature
    Ran Change Method Signature refactoring operation on playAction() method
    replacing void with Boolean (return type) and added a String parameter
    "msg". After the refactor operation multiple other .java files had to be
    manually coded due to an error of them not having a return statement for
    the new return type Boolean.
    Ran Junit Test and all unit tests returned valid.

--- src/edu/towson/cis/cosc442/project1/monopoly/GoCell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/GoCell.java
@@ -6,7 +6,8 @@ public class GoCell extends Cell {
 		setAvailable(false);
 	}
 
-	public void playAction() {
+	public Boolean playAction(String msg) {
+		return false;
 	}
 	
 	void setName(String name) {
